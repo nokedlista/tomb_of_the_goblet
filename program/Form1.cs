171feefbf05846c8e3271b8f137c78ec4e34972e
Endrődi Kálmán <kendodik@gmail.com>
2025-01-03 19:22:38 +0100

    fields data done, no debug, not working

--- program/Form1.cs
+++ program/Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static program.Map;
 
 namespace program
 {
@@ -15,6 +17,31 @@ namespace program
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            List<Map> maps = new List<Map>();
+            StreamReader sr = new StreamReader("mapData.txt");
+            List<string> data = new List<string>();
+            while (!sr.EndOfStream)
+            {
+                string line;
+                data.Clear();
+                do
+                {
+                    line = sr.ReadLine();
+                    data.Add(line);
+
+                } while (line != " ");
+                int x = line.Length;
+                int y = data.Count / x;
+                Map map = new Map(x, y, data);
+                maps.Add(map);
+            }
+            sr.Close();
+
+            label1.Text += maps[0].displayFieldType();
         }
     }
 }
--- program/Map.cs
+++ program/Map.cs
@@ -12,42 +12,132 @@ namespace program
 {
     internal class Map
     {
-        public enum FieldType
-        {
-            Emty = 0,
-            //simple walls:
-            WT = 1,
-            WB = 2,
-            WR = 3,
-            WL = 4,
-            WLT = 5,
-            WRT = 6,
-            WRB = 7;
-            WLB = 8;
-            WLR = 9;
-            WTB = 10,
-            WLRT = 11,
-            WLTB = 12,
-            WRTB = 13,
-            WLRB = 14,
-            WLRTB = 15,
-            Doted = 16,
-            Portal = 17,
-            Gate = 18,
-            Caracter = 19,
-            Spike = 20,
-            TimedSpike = 21,
-        }
+
 
         private int x { set; get; }
         private int y { set; get; }
         private int end { set; get; }
+        private List<string> data { set; get; }
+        private Field[,] fields { set; get; }
+        //private pictureBox[,] fieldsDisplay { set; get; } 
 
-        public Map(int x, int y, int end)
+        public Map(int x, int y, List<string> data)
         {
             this.x = x;
             this.y = y;
-            this.end = end;
+            this.data = data;
+            BuildMap();
+        }
+
+        private void BuildMap() 
+        {
+            for (int i = 0; i < y; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    Field field = new Field(data[i * j + j]);
+                }
+            }
+        }
+
+        public string displayFieldType()
+        {
+            List<string> display = new List<string>();
+            for (int i = 0; i < fields.GetLength(0); i++)
+            {
+                for (int j = 0; j < fields.GetLength(1); j++)
+                {
+                   display.Add(fields[i,j].Type + ", ");
+                }
+                display.Add("\n");
+            }
+
+            return display.ToString();
+        }
+    }
+
+    internal class Field
+    {
+        public enum FieldType
+        {
+            Emty,
+            Wall,
+            Doted,
+            Gate,
+            Portal,
+            Caracter,
+            Spike,
+            TimedSpike,
+            Shooting,
+            Bulet,
+        }
+
+        /* public enum WallGrafic
+        {
+            Top,
+            Bottom,
+            Right,
+            Left,
+            TopRight,
+            TopLeft,
+            BottomRight,
+            BottomLeft,
+            RightLeft,
+            TopBottom,
+            TopRightLeft,
+            TopBottomLeft,
+            TopBottomRight,
+            BottomRightLeft,
+            TopBottomLeftRight,
+        }
+
+        public enum SpikeGrafic
+        {
+            Wall1,
+            Wall2,
+            Wall3,
+            Wall4,
+            Wall9Left,
+            Wall5Top,
+        }
+
+        public enum TimedSpikeGrafic
+        {
+            Wall7,
+            Wall15,
+            Wall13Bottom,
+            Wall8Bottom,
+        } */
+
+        private FieldType type;
+
+        public FieldType Type
+        {
+            get { return type; }
+            set  //only doted fields are able to change to emty
+            {
+                if (type == FieldType.Doted)
+                {
+                    type = FieldType.Emty;
+                }
+                else
+                {
+                    throw new Exception("A mezõ típusa nem módosítható");
+                }
+            }
+        }
+        public Field(string typeData)
+        {
+            switch (typeData[0])
+            {
+                case '0': type = FieldType.Emty; break;
+                case 'T': type = FieldType.TimedSpike; break;
+                case 'S': type = FieldType.Spike; break;
+                case 'd': type = FieldType.Doted; break;
+                case 'G': type = FieldType.Gate; break;
+                case 'P': type = FieldType.Spike; break;
+                default: type = FieldType.Wall; break;
+            }
         }
     }
 }
